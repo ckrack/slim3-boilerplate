d6fc7e4f11313b3c77c5ba0d4457f8b0a9023cb3
Clemens Krack <ckrack@gmail.com>
2017-02-13 20:19:01 +0100

    Use h5bp index.html and add js/css from h5bp
    - Rewrite the Slim index.php to use h5bp index.html
    - Add normalize.scss
    - Add h5bp css into main.scss
    - Add h5bp modernizr build

--- src/templates/index.php
+++ src/templates/index.php
@@ -1,38 +1,34 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
     <head>
-        <meta charset="utf-8"/>
-        <title>Slim 3</title>
-        <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
-        <style>
-            body {
-                margin: 50px 0 0 0;
-                padding: 0;
-                width: 100%;
-                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-                text-align: center;
-                color: #aaa;
-                font-size: 18px;
-            }
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Slim3 Boilerplate</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            h1 {
-                color: #719e40;
-                letter-spacing: -3px;
-                font-family: 'Lato', sans-serif;
-                font-size: 100px;
-                font-weight: 200;
-                margin-bottom: 0;
-            }
-        </style>
+        <!-- Short description of the page (limit to 150 characters) -->
+        <!-- In *some* situations this description is used as a part of the snippet shown in the search results. -->
+        <meta name="description" content="">
+
+        <link rel="stylesheet" href="/css/main.css">
+
+        <link rel="author" href="humans.txt">
+
+        <script src="/js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
-        <h1>Slim</h1>
-        <div>a microframework for PHP</div>
+        <h1>Slim3 Boilerplate</h1>
+        <div>a microframework for PHP with opinionated defaults</div>
 
         <?php if (isset($name)) : ?>
             <h2>Hello <?= htmlspecialchars($name); ?>!</h2>
-        <?php else: ?>
-            <p>Try <a href="http://www.slimframework.com">SlimFramework</a>
+        <?php else : ?>
+            <p>Try <a href="/SlimFramework">SlimFramework</a>
         <?php endif; ?>
+        <script src="/js/main.js"></script>
     </body>
 </html>
+
